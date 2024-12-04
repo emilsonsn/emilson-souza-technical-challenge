8e8461b353d173533f911ac4127fac5f6a6d61f1
emilsonsn <emilsonsn2@gmail.com>
2024-12-04 12:46:23 -0300

    feat: implementação documentação utilizando scribe

---
+++ .scribe/intro.md
@@ -0,0 +1,14 @@
+# Introduction
+
+Esta API fornece acesso aos recursos do sistema, como municípios, estados e muito mais.
+
+<aside>
+    <strong>Base URL</strong>: <code>http://127.0.0.1:8000</code>
+</aside>
+
+Esta documentação fornece todas as informações necessárias para utilizar a API de Municípios.
+
+<aside>O objetivo desta API é atender à submissão de um desafio técnico para uma vaga de desenvolvedor. Ela permite listar municípios de uma UF específica, aplicar filtros, e realizar paginação nos resultados. A API também implementa caching para otimizar o desempenho.</aside>
+
+Por favor, utilize os exemplos de código fornecidos à direita (ou abaixo, em dispositivos móveis) para integração com diferentes linguagens de programação. Você pode alternar a linguagem no menu superior.
+
--- app/Http/Controllers/MunicipalityController.php
+++ app/Http/Controllers/MunicipalityController.php
@@ -13,6 +13,57 @@ class MunicipalityController extends Controller
         $this->municipalityService = $municipalityService;
     }
 
+    /**
+     * Retorna a lista de municípios.
+     *
+     * @group Municípios
+     * 
+     * @urlParam uf string required A sigla da UF (estado). Example: pb
+     * @queryParam search_term string optional O termo para buscar municípios. Example: João
+     * @queryParam take int optional O número de itens por página. Default: 10. Example: 10
+     * @queryParam page int optional A página atual. Default: 1. Example: 1
+     *
+     * @response 200 {
+     *    "status": true,
+     *    "data": {
+     *        "current_page": 1,
+     *        "data": [
+     *            {
+     *                "name": "São João Do Rio Do Peixe",
+     *                "ibge_code": "2500700"
+     *            },
+     *        ],
+     *        "first_page_url": "http://127.0.0.1:8000/api/municipios/pb?page=1",
+     *        "from": 1,
+     *        "last_page": 1,
+     *        "last_page_url": "http://127.0.0.1:8000/api/municipios/pb?page=1",
+     *        "links": [
+     *            {
+     *                "url": null,
+     *                "label": "&laquo; Previous",
+     *                "active": false
+     *            },
+     *            {
+     *                "url": "http://127.0.0.1:8000/api/municipios/pb?page=1",
+     *                "label": "1",
+     *                "active": true
+     *            },
+     *            {
+     *                "url": null,
+     *                "label": "Next &raquo;",
+     *                "active": false
+     *            }
+     *        ],
+     *        "next_page_url": null,
+     *        "path": "http://127.0.0.1:8000/api/municipios/pb",
+     *        "per_page": 4,
+     *        "prev_page_url": null,
+     *        "to": 3,
+     *        "total": 3
+     *    }
+     * }
+     */
+
     public function index(Request $request, string $uf)
     {
         return response()->json($this->municipalityService->getMunicipalities($request, $uf));
